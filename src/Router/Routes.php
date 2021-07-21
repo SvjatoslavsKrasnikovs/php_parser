4e8cb9d0c71fef82414a032072b3d136d603a54b
Slava <svjatoslavs.krasnikovs@gmail.com>
2021-07-21 15:50:56 +0300

    old router

--- src/Router/Routes.php
+++ src/Router/Routes.php
@@ -13,5 +13,9 @@ $router->get('/', function() {
 
 // This will be the main route for now;
 $router->get('/api/parse_url', function($request) {
-    return json_encode($request);
+    // var_dump($request->getBody());
+    // return json_encode($request->getBody());
+    header('Content-Type: application/json');
+    $arr = array ('a'=>1,'b'=>2,'c'=>3,'d'=>4,'e'=>5);
+    echo json_encode($arr); // {"a":1,"b":2,"c":3,"d":4,"e":5}
 });
